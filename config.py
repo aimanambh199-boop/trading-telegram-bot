d0d19019b7560957d9568f9640d59aaa1136fcfc
aimanambh199-boop <aimanambh199@gmail.com>
2026-03-10 17:20:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 TELEGRAM_TOKEN = "8608199250:AAGzNIgOAikE0z4D7xjnCL4aMY61jjhsWOk"
 
 # Telegram Channel Username (jahan signals post honge)
-CHANNEL_ID = "@arhan quotex signal bot"
+CHANNEL_ID = "@arhan_quotex_signal_bot"
 
 # Twelve Data API Key
 TWELVE_DATA_API_KEY = "98521af3ba4d4620985262aaef2caf7b"
@@ -20,3 +20,4 @@ PAIRS = [
     "GBPJPY",
     "EURGBP"
 ]
+
